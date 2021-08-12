8ea2173f745a6b5c32ab76762c231188fd2fd290
Ozan Honamlıoğlu <ozanhonamlioglu@hotmail.com>
2021-08-12 08:57:57 +0300

    Initial ui setup

---
+++ Youtube-UIKit-API/https/HomeFeedService.swift
@@ -0,0 +1,16 @@
+//
+//  homefeed.swift
+//  Youtube-UIKit-API
+//
+//  Created by ozan honamlioglu on 12.08.2021.
+//
+
+import Foundation
+
+class HomeFeedService {
+    
+    func fetchHomeList() {
+        
+    }
+    
+}
---
+++ Youtube-UIKit-API/utils/config.swift
@@ -0,0 +1,10 @@
+//
+//  config.swift
+//  Youtube-UIKit-API
+//
+//  Created by ozan honamlioglu on 12.08.2021.
+//
+
+import Foundation
+
+let BASE_URL = "http://localhost:3000/"
