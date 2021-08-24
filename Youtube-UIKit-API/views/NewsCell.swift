572e34da448c5627f3bfe813cf0cb9e08d459e75
ozan honamlioglu <ozan.honamlioglu@inveon.com>
2021-08-24 18:23:38 +0300

    tabbar designed
    news cell almost complete

--- Youtube-UIKit-API/views/NewsCell.swift
+++ Youtube-UIKit-API/views/NewsCell.swift
@@ -8,16 +8,26 @@
 import UIKit
 
 class NewsCell: UITableViewCell {
-
+    
+    // MARK: - OUTLETS
+    @IBOutlet weak var profilePicture: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        setup()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//    }
+    
+    // MARK: - HANDLERS
+    
+    private func setup() {
+        profilePicture.layer.cornerRadius = 20
+        
     }
     
 }
